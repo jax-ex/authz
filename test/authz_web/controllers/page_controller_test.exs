f79211dc32cdff6a189df5d078d89fd47051f17b
Micah Woods <micahwoods@gmail.com>
2023-03-09 18:55:30 -0500

    Remove uneeded tests for focus

--- test/authz_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule AuthzWeb.PageControllerTest do
-  use AuthzWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Blogging done right!"
-  end
-end
